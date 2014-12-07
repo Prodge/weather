d1093460a04b8d60bd437aaf1f6cd08ea45b790d
prodge <prodge@prodge.net>
2014-12-07 21:09:49 +0800

    Added support for accessing values from xml

--- weather.c
+++ weather.c
@@ -54,9 +54,66 @@ struct string scrape(char *url, struct string s){
     return s;
 }
 
+//Returns the value from the xml that is pointed to by the passed variables
+char *getValue(char *class, char *type, int classLen, int typeLen, struct string s){
+    int pos = 0;
+    //searching for class
+    for(int i=0; i<s.len; i++){
+        if(s.ptr[i] == '<'){
+            bool match = true;
+            for(int x=0; x<classLen; x++){
+                if(s.ptr[i+x+1] != class[x]){
+                    match = false;
+                    break;
+                }
+            }
+            if(match){
+                pos = i+classLen;
+                break;
+            }
+        } 
+    }
+    if(pos == 0){
+        fprintf(stderr, "Could not find given class in function getValue()\n");
+        exit( EXIT_FAILURE );
+    }
+    //checking for type
+    for(int i=pos; i<s.len; i++){
+        if(s.ptr[i] == '>'){
+            fprintf(stderr, "Could not find given type in function getValue()\n");
+            exit( EXIT_FAILURE );
+        }
+        bool match = true;
+        for(int x=0; x<typeLen; x++){
+            if(s.ptr[i+x] != type[x]){
+                match = false;
+                break;
+            }
+        }
+        if(match){
+            pos = i + typeLen + 2; //set pos to the start of the value
+            break;
+        }
+    }
+    //extracting value
+    int i = 0;
+    char *value;
+    value = (char *) malloc(1);
+    while(s.ptr[pos+i] != '"'){
+        value = (char *) realloc(value, i+1);
+        value[i] = s.ptr[pos+i];
+        i++;
+    }
+    value[i] = '\0';
+    return value;
+}
+
 int main(){
     char *url = "http://api.openweathermap.org/data/2.5/weather?q=perth&mode=xml";
     struct string s = scrape(url, s);
-    printf("Output:\n%s\n", s.ptr);
+    //printf("Output:\n%s\n", s.ptr);
+    printf("Temperature: %s\n", getValue("temperature", "value", 11, 5, s));
+    printf("Wind Speed: %s\nWind Direction: %s\n", getValue("speed", "name", 5, 4, s), getValue("direction", "code", 8, 4, s));
+    printf("Weather: %s\n", getValue("weather", "value", 7, 5, s));
     free(s.ptr);
 }
