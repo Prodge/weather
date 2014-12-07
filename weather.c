e5b0bf47d11e7aecfc914ef44c6cb4dc6c45552b
prodge <prodge@prodge.net>
2014-12-07 18:18:22 +0800

    Can now scrape data to string from given url

--- weather.c
+++ weather.c
@@ -5,16 +5,58 @@
 #include <string.h>
 #include <curl/curl.h>
 
-int main(){
-    URL *curl;
-    URLcode xml;
+struct string {
+    char *ptr;
+    size_t len;
+};
+
+
+void init_string(struct string *s) {
+    s->len = 0;
+    s->ptr = malloc(s->len+1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    s->ptr[0] = '\0';
+}
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
+{
+    size_t new_len = s->len + size*nmemb;
+    s->ptr = realloc(s->ptr, new_len+1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "realloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(s->ptr+s->len, ptr, size*nmemb);
+    s->ptr[new_len] = '\0';
+    s->len = new_len;
+    return size*nmemb;
+}
+
+struct string scrape(char *url, struct string s){
+    CURL *curl;
+    CURLcode res;
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=perth&mode=xml");
-        xml = curl_easy_perform(curl);
-        if(xml != CURLE_OK){
+        init_string(&s);
+        //curl_easy_setopt(curl, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=perth&mode=xml");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         }
         curl_easy_cleanup(curl);
     }
+    return s;
+}
+
+int main(){
+    char *url = "http://api.openweathermap.org/data/2.5/weather?q=perth&mode=xml";
+    struct string s = scrape(url, s);
+    printf("Output:\n%s\n", s.ptr);
+    free(s.ptr);
 }
